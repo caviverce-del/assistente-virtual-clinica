71e3a9024d3e06cc98bdb576035dcbb2c2d513f9
Visão Solidária <caviver.ce@gmail.com>
2026-04-29 16:34:23 -0300

    Melhora fluxo humanizado de agendamento5

--- app.py
+++ app.py
@@ -354,9 +354,7 @@ Mensagem do paciente:
             return jsonify({
                 "resposta": """O valor da consulta do Visão Solidária fica R$150,00 
 
-            O valor dos exames podem variar conforme tipo de exame.
-
-            Você deseja agendar online ou falar com uma atendente?""",
+        O valor dos exames podem variar conforme tipo de exame.""",
                                         "transferir": False
             })
 
